6c690f5a1a5bf20c1e8ef55b100235fbc68a3141
Alwin Tareen <altareen@gmail.com>
2021-02-23 19:40:28 -0500

    Updated requirements.txt

--- application.py
+++ application.py
@@ -18,6 +18,8 @@ import os, datetime
 #from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 #from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
